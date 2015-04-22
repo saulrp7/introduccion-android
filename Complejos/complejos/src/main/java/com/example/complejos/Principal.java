f7396c8c290bff56c0dee030aac8c6e6472260e6
Saul Rueda <saulrp7@gmail.com>
2015-04-21 23:29:13 -0430

    Modificacion a Principal

--- Complejos/complejos/src/main/java/com/example/complejos/Principal.java
+++ Complejos/complejos/src/main/java/com/example/complejos/Principal.java
@@ -5,10 +5,13 @@ package com.example.complejos;
  */
 public class Principal {
     public static void main(String[] main) {
-        Complejo z, w;
-        z = new ComplejoAmpliado(-1.5, 0.1);
-        w = new ComplejoAmpliado(-1.2, 0.0);
-        z.suma(w);
-        System.out.println("Complejo Ampliado: " + z.toString());
+        Complejo lista[] = new Complejo[4];
+        lista[0] = new Complejo(-1.5, 3.0);
+        lista[1] = new Complejo(-1.2, -3.0);
+        lista[2] = new ComplejoAmpliado(-1.5,3.0);
+        lista[3] = new ComplejoAmpliado(-1.2,0);
+        for (int i=0; i<lista.length; i++) {
+            System.out.println("Complejo: " + lista[i]);
+        }
     }
 }
